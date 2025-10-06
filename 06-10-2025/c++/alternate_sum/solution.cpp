b6b210ffc7b26dabccc0fc04f58316ca6d4149bd
mahmoud-47 <gysoft.sn@gmail.com>
2025-10-06 20:50:46 +0200

    xxx

--- 06-10-2025/c++/alternate_sum/solution.cpp
+++ 06-10-2025/c++/alternate_sum/solution.cpp
@@ -1,3 +1,42 @@
+/*
+** Problem Description **
+
+You are given an integer number. 
+You need to compute the alternating sum of its digits, starting with addition for the first digit.
+
+That is:
+    result = 1st_digit - 2nd_digit + 3rd_digit - 4th_digit + 5th_digit - ...
+
+Return the final result as an integer.
+
+---
+Example 1
+
+Input :
+num = 1234
+
+Output: -2
+
+Explanation:
+    1 - 2 + 3 - 4 = -2
+
+---
+Example 2
+
+Input :
+num = 98765
+
+Output: 5
+
+Explanation:
+    9 - 8 + 7 - 6 + 5 = 7
+
+Constraints:
+    - 1 <= num <= 10^18
+    - The input number will not have leading zeros.
+    - The result must be returned as an integer.
+*/
+
 #include "solution.h"
 
 long long Solution::alternate_sum(long long num) {
--- 06-10-2025/c++/max_valid_subarray/solution.cpp
+++ 06-10-2025/c++/max_valid_subarray/solution.cpp
@@ -1,3 +1,55 @@
+/**
+ * ** Problem Description **
+
+You are given an array of integers `sizes` and an integer `threshold`.
+
+You need to find the **maximum length of a contiguous subarray** such that:
+
+    (sum of elements in that subarray) - (sum of all elements before the subarray) <= threshold
+
+In other words, for every valid subarray `sizes[i:j]`, 
+check if `sum(sizes[i:j]) - sum(sizes[0:i]) <= threshold`. 
+Among all valid subarrays, return the **maximum length**.
+
+---
+
+Example 1
+
+Input :
+sizes = [2, 1, 3, 2, 4]
+threshold = 5
+
+Output: 3
+
+Explanation:
+    - subarray [2, 1, 3] → sum = 6, previous sum = 0 → 6 - 0 = 6 > 5 (Nooope)
+    - subarray [1, 3, 2] → sum = 6, previous sum = 2 → 6 - 2 = 4 <= 5 (OK) length = 3
+    - subarray [3, 2, 4] → sum = 9, previous sum = 3 → 9 - 3 = 6 > 5 (Nooope)
+    The longest valid subarray has length 3.
+
+---
+
+Example 2
+
+Input :
+sizes = [5, 1, 2, 1, 1, 3]
+threshold = 4
+
+Output: 4
+
+Explanation:
+    - subarray [1, 2, 1, 1] → sum = 5, previous sum = 5 → 5 - 5 = 0 <= 4 (OK) length = 4
+    - no longer subarray satisfies the condition.
+---
+
+Constraints:
+    - 1 <= len(sizes) <= 10^5
+    - 1 <= sizes[i] <= 10^4
+    - 0 <= threshold <= 10^9
+
+Return an integer representing the maximum valid subarray length.
+ */
+
 #include <vector>
 #include "solution.h"
 
